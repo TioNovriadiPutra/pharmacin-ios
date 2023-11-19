bf33bddb4c2b65419aa405eee021fc6be71429e4
Devin <devin.maleke@gmail.com>
2023-11-19 19:18:07 +0700

    Initial Commit

---
+++ Pharmacin/PharmacinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PharmacinApp.swift
+//  Pharmacin
+//
+//  Created by Devin Maleke on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PharmacinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

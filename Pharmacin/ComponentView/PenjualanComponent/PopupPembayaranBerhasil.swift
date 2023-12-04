da2e4f0823784b0e61e411b926b79687e5ca942f
Devin <devin.maleke@gmail.com>
2023-12-04 19:14:15 +0700

    commit

---
+++ Pharmacin/ComponentView/PenjualanComponent/PopupPembayaranBerhasil.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Pharmacin
+//
+//  Created by Devin Maleke on 02/12/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        VStack{
+            Text("Konfirmasi Pembelian")
+        }
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ Pharmacin/ComponentView/StokComponent/StokOpnameDetailList.swift
@@ -0,0 +1,18 @@
+//
+//  StokOpnameDetailList.swift
+//  Pharmacin
+//
+//  Created by Devin Maleke on 02/12/23.
+//
+
+import SwiftUI
+
+struct StokOpnameDetailList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StokOpnameDetailList()
+}

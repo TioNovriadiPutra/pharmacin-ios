df0b07a0f14a7998810a3a76f5c09d01fbe43a50
Devin <devin.maleke@gmail.com>
2023-12-23 18:02:07 +0700

    Update Manajemen View

---
+++ Pharmacin/ComponentView/ManajemenComponent/EditInfoKlinikPopUp.swift
@@ -0,0 +1,18 @@
+//
+//  EditInfoKlinikPopUp.swift
+//  Pharmacin
+//
+//  Created by Devin Maleke on 23/12/23.
+//
+
+import SwiftUI
+
+struct EditInfoKlinikPopUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditInfoKlinikPopUp()
+}
